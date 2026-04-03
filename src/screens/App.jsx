3b8d165cf5e8394bba713cc783541135feaccbad
Guinelo <agnelosouza2007@gmail.com>
2026-04-03 15:42:29 -0400

    fix:Implementando e alterando para integração com o Supabase

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,24 +1,59 @@
-import { useState } from 'react'
-import '../css/App.css'
+import React, {useState} from 'react';
+import '../css/App.css';
+import { supabase } from '../lib/supabase';
+
 
 function App() {
-  function handleLogin() {
-    console.log("Lidando com Login");
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  async function handleLogin() {
+    if (!email || !senha) {
+      alert('Preencha email e senha!');
+      return;
+    }
+
+    const {data, error} = await supabase.auth.signInWithPassword({
+      email,
+      password: senha
+    });
+
+    if (error) {
+      alert('Erro: ' + error.message);
+    } else {
+      alert('Logado!');
+      console.log(data);
+    }
   }
 
   function handleForgetPassword() {
     console.log("Lidando com esqueceu a senha");
   }
 
-  function handleRegisterUser() {
-    console.log("Lidando com cadastrar novo usuário");
+  async function handleRegisterUser() {
+    if (!email || !senha) {
+      alert('Preencha email e senha!');
+      return;
+    }
+    
+    const {data, error} = await supabase.auth.signUp({
+      email,
+      password: senha
+    });
+
+    if (error) {
+      alert('Erro: ' + error.message);
+    } else {
+      alert('Usuário cadastro! Verifique seu email email!');
+      console.log(data);
+    }
   }
 
   return (
     <div className="container">
       <h1>Login</h1>
-      <input type="email" placeholder="Email"></input>
-      <input type="password" placeholder="Senha"></input>
+      <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+      <input type="password" onChange={(e) => setSenha(e.target.value)} placeholder="Senha"></input>
       <button onClick={handleLogin}>Entrar</button>
       <button onClick={handleForgetPassword}>Esqueceu a senha?</button>
       <button onClick={handleRegisterUser}>Cadastrar</button>
